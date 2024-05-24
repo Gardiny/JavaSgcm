ce4a8bb71a506ff92e537bf9e30208c5b11f925c
Gardiny <igor20091983@gmail.com>
2024-05-23 21:01:15 -0300

    dao

--- src/main/webapp/java/br/ufac/sgcm/dao/ConexaoBD.java
+++ src/main/webapp/java/br/ufac/sgcm/dao/ConexaoBD.java
@@ -1,5 +1,18 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.cj.jdbc.ConnectionGroup;
+
 public class ConexaoBD {
-    String url =
-    String usuario = "root";
-    String senha = "root";
+    private String url = "jdbc:mysql://localhost/sgcm";
+    private String usuario = "root";
+    private String senha = "root";
+    Connection con;
+    try{
+    
+    Class.forName("com.mysql.cj.jdbc.Drive");
+    con = DriverManager.getConnection(url, usuario, senha)
+    }catch(SQLException e){
+        e.printStackTrace();
+    }
 }
